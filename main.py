8bd5d0e9deddd73f57a2ab44fd9243944abfd088
muhammad subhan <muhammedsubhan692@gmail.com>
2024-12-04 12:53:45 +0500

    scraping timesjobs

--- main.py
+++ main.py
@@ -1,27 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://subhan.vercel.app/"
+# url = "https://subhan.vercel.app/"
+# response = requests.get(url)
+
+# if response.status_code == 200:
+#     soup = BeautifulSoup(response.text, 'lxml')
+    
+#     title = soup.title.string
+#     print("Page Title:", title)
+
+#     for link in soup.find_all('a'):
+#         print("Link:", link.get('href'))
+    
+#     # link = soup.find_all('a')
+    
+#     # print(link)
+
+   
+#     sections = soup.find_all('section')
+#     for section in sections:
+#         print("Section content:", section.get_text(strip=True))
+# else:
+#     print(f"Failed to retrieve the website. Status code: {response.status_code}")
+
+
+url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=as&searchTextText=USA&txtKeywords=%22Software+Engineer%22%2C&txtLocation=USA"
+
 response = requests.get(url)
 
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'lxml')
     
-    title = soup.title.string
-    print("Page Title:", title)
-
-    for link in soup.find_all('a'):
-        print("Link:", link.get('href'))
+    jobs = soup.find('li',class_='clearfix job-bx wht-shd-bx')
     
-    # link = soup.find_all('a')
-    
-    # print(link)
-
-   
-    sections = soup.find_all('section')
-    for section in sections:
-        print("Section content:", section.get_text(strip=True))
-else:
-    print(f"Failed to retrieve the website. Status code: {response.status_code}")
-
-
+    print(jobs)
